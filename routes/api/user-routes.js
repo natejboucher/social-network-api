b9d78ff9463d9a5313b6f08503d754033803b00c
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-04-01 12:50:45 -0400

    finished all routes and controller for User

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -9,7 +9,6 @@ const {
 
 router.route('/').get(getAllUsers).post(createUser);
 
-router.route(':/id').get(getUserById)
-//.put(updateUser).delete(deleteUser);
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
